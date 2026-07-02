1c771ca4c41b6c532c23bf8a4e8a72665fcdbc25
Suyog Verma <suyogverma0057@gmail.com>
2026-07-02 16:24:38 +0530

    Added real-time YOLO object detection

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import cv2
+from ultralytics import YOLO
 
 cap = cv2.VideoCapture(0)
+model = YOLO("yolov8n.pt")
 
 while True:
     ret, frame = cap.read()
+    results = model(frame,verbose=False)
 
     # Check if frame captured correctly
     if not ret:
         print("Failed to capture frame")
         break
 
-    cv2.imshow("Investigation Support System", frame)
+    annotated_frame = results[0].plot()
+    cv2.imshow("Investigation Support System", annotated_frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
