1099c948c219a08d15a0d42273bda5d935ff2681
Suyog Verma <suyogverma0057@gmail.com>
2026-07-03 21:53:14 +0530

    Added custom person detection and confidence

--- app.py
+++ app.py
@@ -1,20 +1,42 @@
 import cv2
 from ultralytics import YOLO
 
-cap = cv2.VideoCapture(0)
+# Load YOLO model
 model = YOLO("yolov8n.pt")
+
+# Open webcam
+cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
-    results = model(frame, classes=[0], verbose=False)
 
-    # Check if frame captured correctly
     if not ret:
         print("Failed to capture frame")
         break
 
-    annotated_frame = results[0].plot()
-    cv2.imshow("Investigation Support System", annotated_frame)
+    results = model(frame, classes=[0], verbose=False)
+
+    for box in results[0].boxes:
+
+        confidence = box.conf[0]
+
+        if confidence > 0.5:
+
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+            cv2.putText(
+                frame,
+                f"Person: {confidence:.2f}",
+                (x1, y1 - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (0, 255, 0),
+                2
+            )
+
+    cv2.imshow("Investigation Support System", frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
