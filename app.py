530a3ff0b716f85148b79adae044bf38076d8895
Suyog Verma <suyogverma0057@gmail.com>
2026-07-04 15:55:16 +0530

    Added FPS Counter

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 from ultralytics import YOLO
 
 # Load YOLO model
@@ -8,7 +9,9 @@ model = YOLO("yolov8n.pt")
 cap = cv2.VideoCapture(0)
 
 while True:
+    start_time = time.time()
     ret, frame = cap.read()
+    
 
     if not ret:
         print("Failed to capture frame")
@@ -35,6 +38,17 @@ while True:
                 (0, 255, 0),
                 2
             )
+    end_time = time.time()
+    fps = 1/(end_time-start_time)
+    cv2.putText(
+        frame,
+        f"FPS: {fps:.2f}",
+        (20, 40),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        2
+    )
 
     cv2.imshow("Investigation Support System", frame)
 
